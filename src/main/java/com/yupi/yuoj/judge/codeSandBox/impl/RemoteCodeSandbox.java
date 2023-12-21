feb588f02864d24e05330ff2b6fe118debe0cdda
= <=>
2023-12-21 23:28:28 +0800

    :sparkles: feat: 实现远程代码沙箱调用

--- src/main/java/com/yupi/yuoj/judge/codeSandBox/impl/RemoteCodeSandbox.java
+++ src/main/java/com/yupi/yuoj/judge/codeSandBox/impl/RemoteCodeSandbox.java
@@ -1,5 +1,10 @@
 package com.yupi.yuoj.judge.codeSandBox.impl;
 
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONUtil;
+import com.alibaba.excel.util.StringUtils;
+import com.yupi.yuoj.common.ErrorCode;
+import com.yupi.yuoj.exception.BusinessException;
 import com.yupi.yuoj.judge.codeSandBox.CodeSandbox;
 import com.yupi.yuoj.judge.codeSandBox.model.ExecuteCodeRequest;
 import com.yupi.yuoj.judge.codeSandBox.model.ExecuteCodeResponse;
@@ -8,6 +13,12 @@ import com.yupi.yuoj.judge.codeSandBox.model.ExecuteCodeResponse;
  * 远程代码沙箱
  */
 public class RemoteCodeSandbox implements CodeSandbox {
+
+    // 定义鉴权请求头和密钥
+    private static final String AUTH_REQUEST_HEADER = "auth";
+
+    private static final String AUTH_REQUEST_SECRET = "secretKey";
+
     /**
      * 执行代码
      *
@@ -16,6 +27,17 @@ public class RemoteCodeSandbox implements CodeSandbox {
      */
     @Override
     public ExecuteCodeResponse execute(ExecuteCodeRequest executeCodeRequest) {
-        return null;
+        System.out.println("远程代码沙箱");
+        String url = "http://localhost:8080/executeCode";
+        String json = JSONUtil.toJsonStr(executeCodeRequest);
+        String responseStr = HttpUtil.createPost(url)
+                .header(AUTH_REQUEST_HEADER, AUTH_REQUEST_SECRET)
+                .body(json)
+                .execute()
+                .body();
+        if (StringUtils.isBlank(responseStr)) {
+            throw new BusinessException(ErrorCode.API_REQUEST_ERROR, "executeCode remoteSandbox error, message = " + responseStr);
+        }
+        return JSONUtil.toBean(responseStr, ExecuteCodeResponse.class);
     }
 }
